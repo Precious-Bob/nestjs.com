fae6a9459ea2b3a234dfae8d6fd35239ba650afc
Pr3ciousXX <preciousbobmanuel501@gmail.com>
2024-05-31 16:52:26 +0100

    add: console.log

--- scripts.js
+++ scripts.js
@@ -1,13 +1,13 @@
 var Plyr = require('./lib/plyr.polyfilled');
 
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
   var headerHeight = document.querySelector('.page-header').clientHeight;
   var stickyNavbarElement = document.querySelector('.navbar-sticky');
   var offset = 300;
 
   toggleStickyNavbar();
 
-  window.addEventListener('scroll', function() {
+  window.addEventListener('scroll', function () {
     toggleStickyNavbar();
   });
 
@@ -22,7 +22,7 @@ window.addEventListener('load', function() {
       global: true,
     },
   });
-  player.once('play', function() {
+  player.once('play', function () {
     var videoWrapper = document.querySelector('.plyr__video-wrapper');
     videoWrapper.classList.add('hidden-poster');
 
@@ -36,13 +36,13 @@ window.addEventListener('load', function() {
 
   function registerNavigation() {
     var elements = document.querySelectorAll('.nav-wrapper a');
-    elements.forEach(function(el) {
+    elements.forEach(function (el) {
       const href = el.getAttribute('href') || '';
       const isAnchor = href[0] === '#';
       if (!isAnchor) {
         return;
       }
-      el.addEventListener('click', function(event) {
+      el.addEventListener('click', function (event) {
         if (!el.scrollIntoView) {
           return;
         }
@@ -60,7 +60,7 @@ window.addEventListener('load', function() {
   const newsletterEmailInput = document.querySelector('#newsletter-email');
   const formRef = document.querySelector('.newsletter-form form');
   if (newsletterAddButton && newsletterEmailInput) {
-    formRef.addEventListener('submit', function(e) {
+    formRef.addEventListener('submit', function (e) {
       e.preventDefault();
 
       newsletterAddButton.setAttribute('disabled', 'disabled');
@@ -72,11 +72,12 @@ window.addEventListener('load', function() {
         'https://nbdggbnqnrevwg6xlex3st3vpe0nyhiq.lambda-url.us-east-2.on.aws/?token=db1f899025b5a59a76b6b34b2a013893';
       xhr.open('POST', url, true);
       xhr.setRequestHeader('Content-Type', 'application/json');
-      xhr.onreadystatechange = function() {
+      xhr.onreadystatechange = function () {
         newsletterAddButton.classList.add('btn-success');
       };
       const data = JSON.stringify({ email: value });
       xhr.send(data);
+      console.log(data);
     });
   }
 });
